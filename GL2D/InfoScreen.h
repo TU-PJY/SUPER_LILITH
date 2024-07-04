8696c88f38a50740e1dfa0b214a02a3b123c6fea
TU-PJY <viska0319@icloud.com>
2024-07-04 16:25:50 +0900

    info screen에 fmod 라이선스 표기 추가

--- GL2D/InfoScreen.h
+++ GL2D/InfoScreen.h
@@ -8,6 +8,7 @@
 class InfoScreen : public OBJ_BASE {
 private:
 	unsigned int Back{};
+	unsigned int FMOD_Logo{};
 	TextUtil Text;
 	GLfloat BackAlpha{};
 	GLfloat Scroll{};
@@ -15,6 +16,8 @@ private:
 public:
 	InfoScreen() {
 		Back = imageUtil.SetImage("black_front");
+		FMOD_Logo = imageUtil.SetImage("FMOD_logo");
+
 		Text.Init(L"Galiver Sans", FW_BOLD, TRUE);
 
 		Text.SetAlign(Align::Middle);
@@ -26,15 +29,18 @@ public:
 		BackAlpha = std::lerp(BackAlpha, 0.7, FT * 10);
 		Scroll += FT * 0.2;
 
-		if (Scroll >= 2.7)
+		if (Scroll >= 3.4)
 			Scroll = -2.0;
 	}
 
 	void Render() {
+		InitTransform();
+		SetColor(0.0, 0.0, 0.0);
 		AlphaValue = BackAlpha;
+		Scale(ASP(10.0), 10.0);
 		ProcessTransform();
 		imageUtil.Draw(Back);
-		AlphaValue = 1.0;
+		
 
 		Text.Draw(0.0, DivideZoom(0.6 + Scroll, cam.Zoom), 
 			DivideZoom(0.45, cam.Zoom), "SUPER LILITH");
@@ -58,11 +64,22 @@ public:
 		Text.Draw(0.0, DivideZoom(-1.05 + Scroll, cam.Zoom),
 			DivideZoom(0.1, cam.Zoom), "[DJ-Striden] - Sentient");
 
-		Text.Draw(0.0, DivideZoom(-1.35 + Scroll, cam.Zoom),
+		Text.Draw(0.0, DivideZoom(-1.2 + Scroll, cam.Zoom),
 			DivideZoom(0.1, cam.Zoom), "(P) 2015-2022 DJ-Striden");
 
-		Text.Draw(0.0, DivideZoom(-1.65 + Scroll, cam.Zoom),
-			DivideZoom(0.1, cam.Zoom), "(MIT Lisence) Copyright 2024 MATA_");
+		Text.Draw(0.0, DivideZoom(-1.45 + Scroll, cam.Zoom),
+			DivideZoom(0.1, cam.Zoom), "Made using FMOD Studio by Firelight Technologies Pty Ltd.");
+
+		InitTransform();
+		SetColor(1.0, 1.0, 1.0);
+		Translate(0.0, DivideZoom(-1.8 + Scroll, cam.Zoom));
+		ScaleSpot(DivideZoom(imageUtil.Aspect(2000, 679) * 0.5, cam.Zoom), DivideZoom(0.5, cam.Zoom));
+		AlphaValue = 1.0;
+		ProcessTransform();
+		imageUtil.Draw(FMOD_Logo);
+
+		Text.Draw(0.0, DivideZoom(-2.3 + Scroll, cam.Zoom),
+			DivideZoom(0.1, cam.Zoom), "Copyleft 2024 MATA_");
 	}
 };
 
